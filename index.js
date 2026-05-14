a9ac1f55d848962600b6d529896fb4f679977e97
neorai1122 <HNYHNHWRY@gmail.com>
2026-05-14 14:58:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const crypto = require('crypto');
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 
 // ===== הגדרות =====
 const INSTANCE_ID = '7107614702';
